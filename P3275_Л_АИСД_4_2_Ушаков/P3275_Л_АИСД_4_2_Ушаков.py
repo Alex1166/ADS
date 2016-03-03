cb92bdfec332eec4cc13c87220dfc8145e9a47f5
Alex <sasha-ushakov@mail.ru>
2016-03-03 16:21:46 +0300

    2

--- P3275_Л_АИСД_4_2_Ушаков/P3275_Л_АИСД_4_2_Ушаков.py
+++ P3275_Л_АИСД_4_2_Ушаков/P3275_Л_АИСД_4_2_Ушаков.py
@@ -5,19 +5,16 @@ from random import randrange
 
 def convert_matrix(matrix, m):
     for k in range(m):
-        for i in range(len(matrix)):            # проход по строкам
-            for j in range(len(matrix[i])):     # проход по столбцами
-                try:
-                    if matrix[i][j] > matrix[i+1][j]:
-                        r = matrix[i][j]
-                        matrix[i][j] = matrix[i+1][j]
-                        matrix[i+1][j] = r
-                except IndexError:
-                    pass
+        for i in range(len(matrix)-1):              # проход по строкам
+            for j in range(len(matrix[i])):         # проход по столбцами
+                if matrix[i][j] > matrix[i+1][j]:
+                    r = matrix[i][j]
+                    matrix[i][j] = matrix[i+1][j]
+                    matrix[i+1][j] = r
     return matrix
 
 
-def show_matrix(matrix):
+def show_matrix(matrix):    # функция для аккуратного отображения матрицы
     s = [[str(e) for e in row] for row in matrix]
     lens = [max(map(len, col)) for col in zip(*s)]
     fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
@@ -29,7 +26,7 @@ def ui():
     try:
         m = int(input('Введите количество строк: '))        # количество строк
         n = int(input('Введите количество столбцов: '))     # количество столбцов
-        inputMatrix = [[randrange(0, 99) for cell in range(n)] for line in range(m)]       # генерация матрицы
+        inputMatrix = [[randrange(0, 100) for cell in range(n)] for line in range(m)]       # генерация матрицы
         print('Строк: {0}\nСтолбцов: {1}\n'.format(m, n))
         print('Исходная матрица:')
         show_matrix(inputMatrix)
